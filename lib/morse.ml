263a1f5df8c9d1a4243f2b0223bf884a96bff669
Guillaume Huysmans <ghuysmans99@gmail.com>
2019-08-24 01:19:44 +0200

    Dune and ppx_expect

--- lib/morse.ml
+++ lib/morse.ml
@@ -43,26 +43,6 @@ let encode tree word =
 let basic = "ETIANMSURWDKGOHVF-L-PJBXCYZQ"
 let extended = "ETIANMSURWDKGOHVF-L-PJBXCYZQ--54-3---2--+----16=/-----7---8-90"
 
-let () =
-  let usage code =
-    prerr_endline ("usage: " ^ Sys.argv.(0) ^ " [--ext] [--decode] ...");
-    exit code
-  in
-  let tree, enc, args =
-    let parse (tree, enc, arg) = function
-      | "--help" | "-h" -> usage 0
-      | "--ext" -> extended, enc, arg
-      | "--decode" -> tree, false, arg
-      | _ as s -> tree, enc, s :: arg
-    in
-    let args = Array.sub Sys.argv 1 (Array.length Sys.argv - 1) in
-    let tree, enc, args = Array.fold_left parse (basic, true, []) args in
-    tree, enc, List.rev args
-  in
-  let output =
-    if enc then
-      List.map (fun s -> encode tree (String.uppercase_ascii s)) args
-    else
-      List.map (fun s -> decode tree (s ^ " ")) args
-  in
-  List.iter print_endline output
+let%expect_test _ =
+  print_endline (encode basic "SOS");
+  [%expect {|... --- ...|}]
